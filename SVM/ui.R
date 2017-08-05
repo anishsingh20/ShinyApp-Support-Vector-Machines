4d96bb3336ae3d81e7ebdbd08577932095a032c4
Ansih Singh Walia <loveeanish@gmail.com>
2017-08-05 15:40:10 +0530

    adding some styling"

--- SVM/ui.R
+++ SVM/ui.R
@@ -7,22 +7,42 @@ shinyUI(fluidPage(
   
       #setting theme
       theme = shinytheme("yeti"),
-      pageWithSidebar(
-      #the header
-        headerPanel("Support Vector Classifier",windowTitle = "SVM APP"),
       
-      #the sidebar panel
+      pageWithSidebar(
+        
+      #the header
+        headerPanel(
+          h1("Support Vector Classifier",align="center",style="color:black"),
+          windowTitle = "SVM APP"
+        ) ,#close headerpanel
+    
+      
+      #the sidebar panel consisting of the SVM parameters sliders
         sidebarPanel(
-          sliderInput("datapoints",'Number of data points', value = 50, min = 10, max = 1000, step = 5),
+          strong(h2("The SVM parameters",style="color:black")),
+          
+          sliderInput("datapoints",'Number of data points',value = 50, min = 10, max = 1000, step = 5),
+          
           sliderInput("shift","Shift between the classes",value = 2, min = 0, max = 5, step = 0.5),
+          
           sliderInput("cparam","Regularization parameter C",value = 10, min = 0.1, max = 100, step = 0.5),
+          
           sliderInput("gamma","Tuning Paramater Gamma",value = 1, min = 0.001, max = 20, step = 0.05)
         
       ) ,
       
       #the main panel
       mainPanel(
-        "main content"
+        
+         plotOutput("svmplot"),
+         hr(),
+         div(height="400px",width="400px",
+           
+           h3("Confusion Matrix",align="left",style="color:red"),
+           p("The daigonals consist of correct classifications and off-diagonals are incorrectly classified")
+           
+           )
+        
         
       )
         
