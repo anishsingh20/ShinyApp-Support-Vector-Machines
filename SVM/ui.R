2563d33dcdda151e6fe71bcbbb1c7048106f56a0
Ansih Singh Walia <loveeanish@gmail.com>
2017-08-05 13:35:24 +0530

    UI elements added

--- SVM/ui.R
+++ SVM/ui.R
@@ -1,33 +1,25 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
+#Shiny app to create a Support vector machine application 
 library(shiny)
+library(shinythemes) #to load shiny themes
 
-# Define UI for application that draws a histogram
+#creating the UI for the app
 shinyUI(fluidPage(
+    #setting the theme
+    theme=shinytheme("united"),
+    pageWithSidebar(
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
   
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
+    headerPanel("Support Vector Classifier"), ##the title of the web app
+    #sidebarPanel()   
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
+      sliderInput(inputId="datapoints",label="Number of Data points",value=20,min=5,max=1000,step=10)
+      
+    ) ,
     
-    # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+      h4("Confusion Matrix :", align = "left")
     )
-  )
-))
+    
+      
+  
+)))
