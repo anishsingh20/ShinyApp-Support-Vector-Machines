dc3a6648835fe06d0e9a94656cf5e1ae8927a057
Ansih Singh Walia <loveeanish@gmail.com>
2017-08-05 14:16:37 +0530

    UI structure created with slider inputs

--- SVM/ui.R
+++ SVM/ui.R
@@ -4,22 +4,29 @@ library(shinythemes) #to load shiny themes
 
 #creating the UI for the app
 shinyUI(fluidPage(
-    #setting the theme
-    theme=shinytheme("united"),
-    pageWithSidebar(
   
-  
-    headerPanel("Support Vector Classifier"), ##the title of the web app
-    #sidebarPanel()   
-    sidebarPanel(
-      sliderInput(inputId="datapoints",label="Number of Data points",value=20,min=5,max=1000,step=10)
+      #setting theme
+      theme = shinytheme("yeti"),
+      pageWithSidebar(
+      #the header
+        headerPanel("Support Vector Classifier",windowTitle = "SVM APP"),
       
-    ) ,
-    
-    mainPanel(
-      h4("Confusion Matrix :", align = "left")
-    )
-    
+      #the sidebar panel
+        sidebarPanel(
+          sliderInput("datapoints",'Number of data points', value = 50, min = 10, max = 1000, step = 5),
+          sliderInput("shift","Shift between the classes",value = 2, min = 0, max = 5, step = 0.5),
+          sliderInput("cparam","Regularization parameter C",value = 10, min = 0.1, max = 100, step = 0.5),
+          sliderInput("gamma","Tuning Paramater Gamma",value = 1, min = 0.001, max = 20, step = 0.05)
+        
+      ) ,
       
-  
-)))
+      #the main panel
+      mainPanel(
+        "main content"
+        
+      )
+        
+      
+    ) #close page with sidebar
+  )
+)
