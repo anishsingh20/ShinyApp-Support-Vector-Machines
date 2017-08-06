06fdd72ede1fb84b35368240166dee572a341e7f
Ansih Singh Walia <loveeanish@gmail.com>
2017-08-06 15:11:02 +0530

    server logic added

--- SVM/server.R
+++ SVM/server.R
@@ -2,13 +2,24 @@
 
 library(shiny)
 library(e1071)#the SVM package
+library(ElemStatLearn)
+
+#Loading the data
+attach(mixture.example)
 
 # Define server logic 
 shinyServer(
   function(input, output) {
     output$svmplot<-renderPlot(
       {
-        hist(rnorm(input$datapoints))
+        #R-syntax to use the widget elements goes here
+        set.seed(1002) 
+        n<-input$datapoints
+        c<-input$cparam
+        shift<-input$shift
+        gamma<-input$gamma 
+        
+        
         
     }
     )#close renderPlot
